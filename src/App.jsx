d82f80b05e2a6de45e3f082487bc51aa54cf44f1
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-10-05 16:14:36 +0530

    added app state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "components/header";
 import Weather from "components/weather";
 
 import "./sass/main.css"
 
 const App = () => {
+  const [query, setQuery] = useState("")
+  const [weather, setWeather] = useState()
+  const [loading, setLoading] = useState(false)
+
   return (
     <div className="app">
       <Header />
-      <Weather />
+      {weather === undefined ? <Weather /> : ""}
     </div>
   );
 }
