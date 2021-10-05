551f610b1b6f28a3cbe8872323a1288483d8abee
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-10-05 21:54:30 +0530

    created weather forecast card

--- src/components/weather/forecast.jsx
+++ src/components/weather/forecast.jsx
@@ -1,7 +1,22 @@
 import React from "react"
 
 const Forecast = () => {
-    return <h2>Forecast</h2>
+    return (
+        <div id="forecast">
+            <article className="weather-card">
+                <section className="date">Mon, Oct 5</section>
+                <section className="weather-icon">
+                    <img
+                        src={`https://openweathermap.org/img/wn/09d.png`}
+                        alt="weather-icon"
+                    />
+                </section>
+                <section className="temp-minmax">17/10 C</section>
+                <section className="description">moderate rain</section>
+            </article>
+
+        </div>
+    )
 }
 
 export default Forecast
--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import Main from "./main"
+import Forecast from "./forecast"
 
 const Weather = ({ weather }) => {
     return (
         <div id="weather">
+            <Forecast />
             <Main data={weather} />
         </div>
     )
